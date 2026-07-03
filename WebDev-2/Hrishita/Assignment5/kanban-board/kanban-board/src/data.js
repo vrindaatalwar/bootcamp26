67b8a13bb5ba1a21c5200b8bb1f8ff641fa8ae06
patrohrishita2612-droid <patrohrishita2612@gmail.com>
2026-07-03 20:50:12 +0530

    alldone

---
+++ WebDev-2/Hrishita/Assignment5/kanban-board/kanban-board/src/data.js
@@ -0,0 +1,28 @@
+const initialData = {
+  backlog: [
+    {
+      id: 1,
+      title: "Finish React notes"
+    },
+    {
+      id: 2,
+      title: "Read 20 pages"
+    }
+  ],
+
+  growing: [
+    {
+      id: 3,
+      title: "Build Navbar"
+    }
+  ],
+
+  finished: [
+    {
+      id: 4,
+      title: "Install Vite"
+    }
+  ]
+};
+
+export default initialData;
